156dab458a5c22e1ba478c73e13bb185ff510adb
dhanya <pk.dhanya1994@gmail.com>
2026-04-11 20:33:16 +0530

    Re-added compressed video

--- global.d.ts
+++ global.d.ts
@@ -5,6 +5,7 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.webp";
 declare module "*.svg";
+declare module "*.mp4";
 
 declare module "swiper/css";
 declare module "swiper/css/pagination";
